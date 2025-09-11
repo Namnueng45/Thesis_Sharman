3636db1d3478b08746cfc659d40dea5b1e66c7f1
Jengza45 <namnueng.int@damrong.ac.th>
2025-09-12 01:39:58 +0700

    update nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,19 @@
-import { Link } from "react-router-dom";
+import React from "react";
 
-function Navbar() {
-  return (
-    <nav style={styles.nav}>
-      <Link to="/" style={styles.link}>
-        Home
-      </Link>
-      <Link to="/about" style={styles.link}>
-        About
-      </Link>
-      <Link to="/contact" style={styles.link}>
-        Contact
-      </Link>
-    </nav>
-  );
-}
+export function Navbar() {}
 
-const styles = {
-  nav: {
-    backgroundColor: "#333",
-    padding: "1rem",
-    display: "flex",
-    gap: "1rem",
-  },
-  link: {
-    color: "#fff",
-    textDecoration: "none",
-    fontWeight: "bold",
-  },
-};
+// const styles = {
+//   nav: {
+//     backgroundColor: "#333",
+//     padding: "1rem",
+//     display: "flex",
+//     gap: "1rem",
+//   },
+//   link: {
+//     color: "#fff",
+//     textDecoration: "none",
+//     fontWeight: "bold",
+//   },
+// };
 
 export default Navbar;
