b1c6d44e3d636a8066e50f8a074eae8ce82b1b6f
Jengza45 <namnueng.int@damrong.ac.th>
2025-09-14 04:08:21 +0700

    Update sectionHero

--- README.md
+++ README.md
@@ -11,7 +11,43 @@ Currently, two official plugins are available:
 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
 
-คำสั่งรัน
+คำสั่งในการ push ขึ้น GIT
 git add .
-git commit -m "update" 
+git commit -m "update"
 git push
+
+Layout respoonsive
+ถ้าอยากให้ dev ขยายตามหน้าจอ 3 วิธี
+
+1. Media Queries
+   Ex.
+   @media (min-width: 768px) {
+   .container {
+   font-size: 16px; /_ Tablet _/
+   }
+   }
+   หรือ
+   <div class="text-sm sm:text-base md:text-lg lg:text-xl">
+   Responsive Text
+   </div>
+2. Flexible Layout
+Flexible Layout (Flex/Grid)
+Ex.
+<div class="flex flex-wrap gap-4">
+    <div class="flex-1 bg-blue-300">Box 1</div>
+    <div class="flex-1 bg-blue-400">Box 2</div>
+    <div class="flex-1 bg-blue-500">Box 3</div>
+</div>
+3. Fluid / Relative Units
+   (%, vw, vh, rem, em แทน px)
+   Ex. w-[60%] max-w-[750px]
+
+ถ้าเป็นรูปใช้
+Responsive Images
+Ex. <img src="image.jpg" class="w-full max-w-[400px] h-auto" />
+
+++ การใช้ flex ++
+
+1. เรียงแกน x ใช้ content , justify-center justifly-....
+2. แนวตั้ง y ใช้ item , items-center items-....
+3. ตัวหนังสื้อให้อยู่ตามที่ต้องการใช้ text-center text-....
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,37 +1,107 @@
 import "./home.css";
+import bgsection1 from "../assets/section1.jpg";
+
 function Home() {
   return (
-    <>
-      <div className="flex justify-between items-center p-6">
-        <div className="text-2xl font-bold ">LOGO</div>
-        <ul className="navbar flex justify-between gap-2 border-1 text-center w-[60%] max-w-[800px]">
-          {/* max-w-[600px] min-w-[350px] */}
-          <li className="flex-1 min-w-[80px] max-w-[150px] border-1">
-            <a href="#synopsis">เรื่องย่อ</a>
+    <section className="relative h-screen">
+      {/* Background image and img parallax*/}
+      <div
+        className="absolute w-[102%] h-full z-0 bg-cover bg-center brightness-70 blur-[5px] top-0 left-[-10px] "
+        style={{ backgroundImage: `url(${bgsection1})` }}
+      ></div>
+
+      {/* บ้าน */}
+      <img
+        src="/public/img/parallax/homesick.png"
+        alt="house"
+        className="absolute bottom-0 left-1/2 -translate-x-1/2 z-5 w-[60%]"
+      />
+
+      {/* ต้นไม้ */}
+      <img
+        src="/public/img/parallax/tre1.png"
+        alt="tree"
+        className="absolute bottom-0 left-[15%] z-20"
+      />
+
+      {/* ต้นไม้ */}
+      <img
+        src="/public/img/parallax/tre2.png"
+        alt="tree"
+        className="absolute bottom-0 left-[15%] z-20"
+      />
+
+      {/* พื้น */}
+      <img
+        src="/public/img/parallax/ground.png"
+        alt="ground"
+        className="absolute bottom-0 left-0 w-full z-30"
+      />
+
+      {/* Overlay gradient ดำ → โปร่งใส */}
+      <div className="absolute z-99 top-0 left-0 w-full h-50 bg-gradient-to-b from-black/100 via-black/70 to-transparent"></div>
+
+      {/* Nav อยู่บน background */}
+      <nav className="relative z-100 flex flex-wrap justify-between items-center p-6 pr-15">
+        <div className="logosvg flex-1">
+          <img
+            src="/public/img/logo/sharmanLogo.svg"
+            alt="logosharman"
+            // style={{ width: "93px", height: "42px" }}
+          />
+        </div>
+        <ul className="navbar flex flex-1 justify-between text-center max-w-[550px]">
+          <li>
+            <a href="#synopsis" className="text-white">
+              เรื่องย่อ
+            </a>
           </li>
-          <li className="flex-1 min-w-[80px] max-w-[150px]">
-            <a href="#characters">ตัวละคร</a>
+          <li>
+            <a href="#characters" className="text-white">
+              ตัวละคร
+            </a>
           </li>
-          <li className="flex-1 min-w-[80px] max-w-[150px]">
-            <a href="#trailer">ตัวอย่าง</a>
+          <li>
+            <a href="#trailer" className="text-white">
+              ตัวอย่าง
+            </a>
           </li>
-          <li className="flex-1 min-w-[80px] max-w-[150px]">
-            <a href="#gallery">รูปภาพ</a>
+          <li>
+            <a href="#gallery" className="text-white">
+              รูปภาพ
+            </a>
           </li>
-          <li className="flex-1 min-w-[80px] max-w-[150px]">
-            <a href="#results">ผลลัพธ์</a>
+          <li>
+            <a href="#results" className="text-white">
+              ผลลัพธ์
+            </a>
           </li>
-        </ul>{" "}
+        </ul>
+      </nav>
+
+      {/* Content ของ hero */}
+      <div className="relative z-10 flex flex-col items-center justify-center h-[80%] ">
+        <img
+          src="/public/img/logo/logogo.svg"
+          alt="logosharman"
+          className="absolute z-100 top-[30%] left-1/2 -translate-x-1/2 -translate-y-1/2"
+        />
+        <div className="text-center text-white mt-[300px] tracking-widest">
+          <p className="font-[100]">ภาพยนตร์มีเรื่องนี้มีรูปแบบ Interactive</p>
+          <p className="font-[100]">
+            ผู้ชมจะได้มีส่วนร่วมกำหนดเส้นทางและชะตากรรมของเรื่อง..
+          </p>
+          <br />
+          <p className="font-[100]">
+            ทุกการตัดสินใจที่คุณเลือก...จะเปลี่ยนตอนจบไปตลอดกาล
+          </p>
+        </div>
+        <button className="buttonPlay tracking-widest border-2 p-3 mt-15 text-white rounded-lg pl-20 pr-20 text-lg font-bold">
+          ดูภาพยนต์
+        </button>
       </div>
-    </>
+    </section>
   );
 }
-// const styles = {
-//   bg: {
-//     display: "flex",
-//     width: "100%",
-//     // height: "230px",
-//   },
-// };
 
 export default Home;
